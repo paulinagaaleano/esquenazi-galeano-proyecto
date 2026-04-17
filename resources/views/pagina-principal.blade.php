b3aa066a1e7fd4d0f95a6090452f954a7d35a6ee
BrenEsq <brendaesquenazipersonal@gmail.com>
2026-04-17 15:07:27 -0300

    Arreglos

--- resources/views/pagina-principal.blade.php
+++ resources/views/pagina-principal.blade.php
@@ -28,7 +28,7 @@
 <div id="carouselAura" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active" data-bs-interval="2000">
-            <img src="/img/car2.png" class="d-block w-100" alt="Promoción 1" style="height: 500px; object-fit: cover;">
+            <img src="/img/Carrusel/car2.png" class="d-block w-100" alt="Promoción 1" style="height: 500px; object-fit: cover;">
         </div>
         <div class="carousel-item" data-bs-interval="2000">
             <img src="/img/car1.png" class="d-block w-100" alt="Promoción 2" style="height: 500px; object-fit: cover;">
